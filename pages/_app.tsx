5b3f3e2682cf9a30260c5bfdda354bc108b8dfea
Sina Syrous <sina@shapegames.com>
2021-09-24 14:23:48 +0200

    Create initial setup of app with recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import DebugObserver from "../components/DebugObserver";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <DebugObserver />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
-export default MyApp
+
+export default MyApp;
